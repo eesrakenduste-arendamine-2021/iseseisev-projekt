d8ab3ea4d3bd8df3a0afb9f31f0bdbcbed3b6059
Hans-Märten Liiu <70939468+hansm4543@users.noreply.github.com>
2021-05-08 14:00:25 +0300

    update

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function k2ik(k2si){
     var cScore = document.getElementById('computerScore').innerHTML;
     var tieScore = document.getElementById('tieScore').innerHTML;
     
-    var x = Math.floor(Math.random()* 6);     
+    var x = Math.floor(Math.random()* 3);     
     var arvutik2ik = arvutivalik[x];
     console.log("Player: "+k2si);
     console.log("Computer: "+arvutik2ik);
@@ -19,12 +19,18 @@ function k2ik(k2si){
         if(k2si == 'kivi'){
             document.getElementById("playerchoice").className = "fas fa-hand-rock"; 
             document.getElementById("computerchoice").className = "fas fa-hand-rock";
-        }else if(k2si == 'paber'){
+            document.getElementById('playerchoice').style.backgroundColor = 'lightblue';
+            document.getElementById('computerchoice').style.backgroundColor = 'lightblue';
+        }else if(k2si == 'paper'){
             document.getElementById("playerchoice").className = "fas fa-hand-paper"; 
             document.getElementById("computerchoice").className = "fas fa-hand-paper";
+            document.getElementById('playerchoice').style.backgroundColor = 'lightblue';
+            document.getElementById('computerchoice').style.backgroundColor = 'lightblue';
         }else if(k2si == 'k22rid'){
             document.getElementById("playerchoice").className = "fas fa-hand-scissors"; 
             document.getElementById("computerchoice").className = "fas fa-hand-scissors";
+            document.getElementById('playerchoice').style.backgroundColor = 'lightblue';
+            document.getElementById('computerchoice').style.backgroundColor = 'lightblue';
         }
 
     }else if(k2si == 'kivi'){
@@ -32,13 +38,17 @@ function k2ik(k2si){
             console.log("sa valisid kivi ja arvuti valis k22rid!");
             var integer = parseInt(pScore, 10);
             document.getElementById('playerScore').innerHTML = integer+1;
+            document.getElementById('playerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('computerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-rock"; 
             document.getElementById("computerchoice").className = "fas fa-hand-scissors"; 
         }else{
-            console.log("sa valisid kivi ja arvuti valis paberi!");
+            console.log("sa valisid kivi ja arvuti valis paperi!");
             var integer = parseInt(cScore, 10);
             document.getElementById('computerScore').innerHTML = integer+1;
+            document.getElementById('computerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('playerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-rock"; 
             document.getElementById("computerchoice").className = "fas fa-hand-paper"; 
@@ -46,26 +56,32 @@ function k2ik(k2si){
 
     }else if(k2si == 'paper'){
         if(arvutik2ik == "kivi"){
-            console.log("sa valisid paberi ja arvuti valis kivi!");
+            console.log("sa valisid paperi ja arvuti valis kivi!");
             var integer = parseInt(pScore, 10);
             document.getElementById('playerScore').innerHTML = integer+1;
+            document.getElementById('playerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('computerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-paper"; 
             document.getElementById("computerchoice").className = "fas fa-hand-rock"; 
         }else{
-            console.log("sa valisid paberi ja arvuti valis k22rid!");
+            console.log("sa valisid paperi ja arvuti valis k22rid!");
             var integer = parseInt(cScore, 10);
             document.getElementById('computerScore').innerHTML = integer+1;
+            document.getElementById('computerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('playerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-paper"; 
             document.getElementById("computerchoice").className = "fas fa-hand-scissors"; 
         }
 
     }else if(k2si== 'k22rid'){
-        if(arvutik2ik == "paber"){
-            console.log("sa valisid k22rid ja arvuti valis paberi!");
+        if(arvutik2ik == "paper"){
+            console.log("sa valisid k22rid ja arvuti valis paperi!");
             var integer = parseInt(pScore, 10);
             document.getElementById('playerScore').innerHTML = integer+1;
+            document.getElementById('playerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('computerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-scissors"; 
             document.getElementById("computerchoice").className = "fas fa-hand-paper"; 
@@ -73,6 +89,8 @@ function k2ik(k2si){
             console.log("sa valisid k22rid ja arvuti valis kivi!");
             var integer = parseInt(cScore, 10);
             document.getElementById('computerScore').innerHTML = integer+1;
+            document.getElementById('computerchoice').style.backgroundColor = 'lightgreen';
+            document.getElementById('playerchoice').style.backgroundColor = 'pink';
 
             document.getElementById("playerchoice").className = "fas fa-hand-scissors"; 
             document.getElementById("computerchoice").className = "fas fa-hand-rock"; 
